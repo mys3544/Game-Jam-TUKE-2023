3ae72c82ea7e5bb469349b537f1fcaa44c2c3ee3
Lukas Cervenak <lukas.cervenak@student.tuke.sk>
2023-04-14 15:23:03 +0200

    enemy shooter update

--- scripts/shoot_and_patrol.gd
+++ scripts/shoot_and_patrol.gd
@@ -10,7 +10,9 @@ func _physics_process(delta):
 	if timer < 0:
 		if target:
 			shoot()
-		timer = 120
+			timer = 120
+		else:
+			timer = 0
 	timer -= 1
 
 # on enter set node to be followed and on leave stop
@@ -18,15 +20,16 @@ func _on_area_2d_body_entered(body):
 	if body.is_in_group("player"):
 		target = body
 
+
 func _on_area_2d_body_exited(body):
 	if body.is_in_group("player"):
 		target = null
 
 func shoot():
-	print("Shot")
 	# instantiate object
 	var bullet = bullet_obj.instantiate()
 	get_parent().add_child(bullet)
 	# set direction
+	bullet.position = position + (position.direction_to(target.position) * 30)
 	shot_dir = target.position - bullet.position
 	bullet.velocity = shot_dir
