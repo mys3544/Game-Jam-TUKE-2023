9a337365481339b6b871b8b51fdc564982f816bd
Lukas Cervenak <lukas.cervenak@student.tuke.sk>
2023-04-14 15:10:38 +0200

    enemy progress so far

---
+++ scripts/shoot_and_patrol.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+var speed = 100
+var shot_dir = Vector2.ZERO
+var target = null
+const bullet_obj = preload("res://scenes/enemy_bullet.tscn")
+var timer = 120
+
+func _physics_process(delta):
+	if timer < 0:
+		if target:
+			shoot()
+		timer = 120
+	timer -= 1
+
+# on enter set node to be followed and on leave stop
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("player"):
+		target = body
+
+func _on_area_2d_body_exited(body):
+	if body.is_in_group("player"):
+		target = null
+
+func shoot():
+	print("Shot")
+	# instantiate object
+	var bullet = bullet_obj.instantiate()
+	get_parent().add_child(bullet)
+	# set direction
+	shot_dir = target.position - bullet.position
+	bullet.velocity = shot_dir
