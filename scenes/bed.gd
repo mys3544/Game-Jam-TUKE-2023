f10a101c25bb9b634fdd2ae4c3c4aed7ef771815
Samey <Samey@DESKTOP-SK>
2023-04-16 01:03:18 +0200

    fix2

--- scenes/bed.gd
+++ scenes/bed.gd
@@ -4,10 +4,9 @@ signal bed_ready
 var empty = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _physics_process(delta):
 	if !empty && get_tree().get_nodes_in_group("hostile").size() == 0:
 		bed_ready.emit()
 		empty = true
 		print(empty)
-		await get_tree().create_timer(5.0).timeout
 	pass
