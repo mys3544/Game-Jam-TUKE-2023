3f211590286cb06aa37e117d29e72300c8a55f1d
Daniel Solc <daniel.solc@student.tuke.sk>
2023-04-15 20:55:43 +0200

    quit button bug

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -8,6 +8,8 @@ extends ColorRect
 func _ready():
 	play_button.pressed.connect(unpause)
 	quit_button.pressed.connect(get_tree().quit)
+	play_button.set_disabled(true)
+	quit_button.set_disabled(true)
 
 func unpause():
 	animator.play("Unpause")
@@ -21,8 +23,10 @@ func pause():
 func _on_character_body_2d_pause():
 	animator.play("Pause")
 	get_tree().paused = true
+	quit_button.set_disabled(false)
 
 
 func _on_character_body_2d_unpause():
 	animator.play("Unpause")
 	get_tree().paused = false
+	quit_button.set_disabled(true)
