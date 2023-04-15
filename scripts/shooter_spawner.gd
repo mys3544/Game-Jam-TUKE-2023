6fb4e3b96d0154bd34bdf6c53c87007414eba68c
Lukas Cervenak <lukas.cervenak@student.tuke.sk>
2023-04-15 17:58:49 +0200

    and corresponding scripts

---
+++ scripts/shooter_spawner.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@onready var normal = preload("res://scenes/hostiles/shooter.tscn")
+@onready var nightmare = preload("res://scenes/hostiles/shooter_nm.tscn")
+
+# later change to globals maybe
+var charges = 1
+var chance = 20
+
+func _ready():
+	var enemy = null
+	for i in range(charges):
+		if randi() % 100 < chance:
+			enemy = nightmare.instantiate()
+		else:
+			enemy = normal.instantiate()
+	self.add_child(enemy)
+	enemy.position = position
