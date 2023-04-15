59c3cd7a0968487c1595cdcb6996ef768517dc34
Lukas Cervenak <lukas.cervenak@student.tuke.sk>
2023-04-15 08:55:38 +0200

    additional files

---
+++ scripts/UI/healthUI.gd
@@ -0,0 +1,31 @@
+extends Control
+
+var max_hearts = 3 : set = set_max_hearts
+var hearts = 3 : set = set_hearts
+var parent = null
+@onready var heart_ui_full = $HeartFull
+@onready var heart_ui_empty = $HeartEmpty
+
+func set_max_hearts(value):
+	# cannot be < 1
+	max_hearts = max(1, value)
+	# draw empty containers (overlapped by full hp)
+	heart_ui_empty.set_size(Vector2(max_hearts * 32, heart_ui_empty.get_rect().size.y))
+
+func set_hearts(value):
+	# between 0 and max_hearts
+	hearts = clamp(value, 0, max_hearts)
+	# draw the hearts / update them
+	heart_ui_full.set_size(Vector2(hearts * 32, heart_ui_full.get_rect().size.y))
+	if hearts <= 0:
+		heart_ui_full.visible = false
+	else:
+		heart_ui_full.visible = true
+
+func _on_player_health_changed(current_hp):
+	set_hearts(current_hp)
+
+func _on_player_health_ready():
+	parent = get_node("/root/Player")
+	set_max_hearts(parent.player_hp.max_health)
+	set_hearts(parent.player_hp.health)
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,6 +2,7 @@ extends Node
 
 const Health = preload("res://scripts/health.gd")
 var player_hp = null
+var i_frame_duration = 1.5
 # signals for UI elements
 signal health_changed(current_hp)
 signal health_ready
@@ -10,17 +11,23 @@ func _ready():
 	player_hp = Health.new()
 	health_ready.emit()
 
+func get_hit():
+	player_hp.sub_hp()
+	health_changed.emit(player_hp.get_health())
+
 # area collisions (pickups/bullets/etc.)
 func _on_area_2d_area_entered(area):
 	if area.is_in_group("potion"):
 		player_hp.add_hp()
 		health_changed.emit(player_hp.get_health())
 	elif area.is_in_group("hostile"):
-		player_hp.sub_hp()
-		health_changed.emit(player_hp.get_health())
+		get_hit()
 
 # character collisions (enemies)
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("hostile"):
-		player_hp.sub_hp()
-		health_changed.emit(player_hp.get_health())
+		get_hit()
+
+func i_frames():
+	get_child(0).get_child(1).disabled = true
+	get_child(0).get_child(4).disabled = true
