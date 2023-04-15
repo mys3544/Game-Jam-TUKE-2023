351837d59d0b5c4eee223b8a54a4761231f30882
Samey <Samey@DESKTOP-SK>
2023-04-15 20:28:57 +0200

    follow nm fix

--- scripts/follow_player_nm.gd
+++ scripts/follow_player_nm.gd
@@ -4,12 +4,20 @@ const Health = preload("res://scripts/health.gd")
 var entity_hp = null
 
 var speed = 200
-var cur_speed = speed
+var prev_speed = speed
+var cur_speed = speed : get = get_cur_speed, set = set_cur_speed
 var direction = Vector2.ZERO
 var to_follow = null
 var backing = false
+var slowed = false
 
 @onready var animation = $AnimationPlayer
+
+func get_cur_speed():
+	return cur_speed
+	
+func set_cur_speed(new_cur_speed):
+	cur_speed = new_cur_speed
 
 func _ready():
 	entity_hp = Health.new()
@@ -43,6 +51,15 @@ func bounce():
 	backing = true
 	await get_tree().create_timer(0.25).timeout
 	backing = false
+	
+func _on_area_2d_area_entered(area):
+	if !slowed && area.is_in_group("projectile") && area.get_cold_pillow_check():
+		slowed = true
+		prev_speed = cur_speed
+		set_cur_speed(cur_speed/100*10)
+		await get_tree().create_timer(1.5).timeout
+		set_cur_speed(prev_speed)
+		slowed = false
 
 func die():
 	queue_free()
